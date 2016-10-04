f0620087d6aebf1cf9be79972c9e3496869c76aa
mroach <mroach@mroach.com>
2016-10-04 21:35:24 +0700

    Show sub-cats and products in cats

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1
   def show
+    @prices = @category.products.map(&:variants).flatten.map { |v| PricesQuery.new(v) }
   end
 
   # GET /categories/new
