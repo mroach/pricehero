c124d27405a9a5eb567699831b6d011d5bd349f6
mroach <mroach@mroach.com>
2016-10-05 23:16:30 +0700

    Set whodunnit on papertrail

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'setting the whodunnit user' do
+    let(:user) { create(:user) }
+
+    before do
+      # Temporarily enable papertrail so this will be tested
+      PaperTrail.enabled = true
+      sign_in user
+    end
+
+    after do
+      # And turn it back off just to be sure
+      PaperTrail.enabled = false
+    end
+
+    it 'sets whodunit for PaperTrail' do
+      get :index
+      expect(response).to be_success # Ensure the sign-in worked
+      expect(PaperTrail.whodunnit).to eq user.id
+    end
+  end
+end
