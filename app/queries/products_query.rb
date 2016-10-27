3e4b25634619c7a6d7becb267f00e157c88ab3d5
mroach <mroach@mroach.com>
2016-10-27 08:46:32 +0700

    Allow other relations on product queries

--- app/queries/products_query.rb
+++ app/queries/products_query.rb
@@ -1,9 +1,10 @@
-# Finds products in a store and the best price for each one
+# Finds products in a thing and the best price for each one
 class ProductsQuery
-  attr_reader :store
+  attr_reader :relation, :relation_name
 
-  def initialize(store)
-    @store = store
+  def initialize(relation, relation_name = nil)
+    @relation = relation
+    @relation_name = relation_name || relation.model_name.element.to_sym
   end
 
   def product_prices
@@ -23,7 +24,7 @@ class ProductsQuery
   # so we can partition by product rather than variant. This allows us
   # to return a list of each product known in a store and the latest price
   def ranked_reports_query
-    Report.joins(:variant).where(store: @store).select(<<-SQL).to_sql
+    Report.joins(:variant).where({relation_name => relation}).select(<<-SQL).to_sql
       reports.*,
       DENSE_RANK() OVER (
         PARTITION BY product_id
