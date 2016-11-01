9a99a4129e18ac6c51fddb84126ccd553952384d
mroach <mroach@mroach.com>
2016-11-02 00:40:44 +0700

    Use alternate country names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,15 @@ module ApplicationHelper
   def country_description(code)
     bits = []
     bits << 'the' if %w(US GB NL CZ AE DO PH CI GM).include?(code)
-    bits << ISO3166::Country[code].try(:name) || code
+    if country = ISO3166::Country[code]
+      # For a couple countries, use an alternate name that sounds better
+      # e.g. Russia instead of Russian Federation
+      # e.g. Macedonia instead of Former Yugoslav Republic of Macedonia
+      # e.g. South Korea instead of Korea, Republic of
+      bits << (%w(MK RU KR).include?(code) ? country.names.first : country.name)
+    else
+      bits << code
+    end
     bits.join(' ')
   end
 end
